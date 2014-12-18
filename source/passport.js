891fe3ac59b33b265d56f5f4c8deac4aab859bf8
asonnenschein <adrian.sonnenschein@gmail.com>
2014-12-17 22:14:10 -0700

    user auth

--- source/passport.js
+++ source/passport.js
@@ -1,5 +1,75 @@
-var LocalStrategy = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy
+  , db = require('./db')
+  ;
 
 function authenticate (passport) {
 
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    db.User.find({where: {id: id}})
+      .then(function (user) {
+        done(null, user);
+      })
+      .catch(function (err) {
+        done(err);
+      })
+    ;
+  });
+
+  passport.use('register',
+    new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    function (req, email, password, done) {
+      db.User.find({where: {username: username}})
+        .then(function (user) {
+          if (!user) {
+            db.User.create({
+              username: username,
+              password: this.generateHash(password)
+            })
+            .then(function (newUser) {
+              return done(null, newUser);
+            })
+            .catch(function (err) {
+              return done(err);
+            })
+          }
+          return done(null, false);
+        })
+        .catch(function (err) {
+          return done(err);
+        })
+      ;
+    }
+  ));
+
+  passport.use('login',
+    new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    function (req, email, password, done) {
+      db.User.find({where: {username: username}})
+        .then(function (user) {
+          if (!user) {}
+            return done(null, false);
+          if (!user.validPassword(password))
+            return done(null, false);
+          return done(null, user);
+        })
+        .catch(function (err) {
+          return done(err);
+        })
+      ;
+    }
+  ));
 }
+
+module.exports = authenticate;
--- source/server.js
+++ source/server.js
@@ -3,7 +3,34 @@ var express = require('express')
   , bodyParser = require('body-parser')
   , cookieParser = require('cookie-parser')
   , passport = require('passport')
-  , server = express();
   ;
+
+var server = express();
+require('./passport')(passport);
+
+server.use(cookieParser());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json({limit: '25mb'}));
+
+server.use(session({secret: 'secret', saveUninitialized: true, resave: true}));
+server.use(passport.initialize());
+server.use(passport.session());
+
+// POST Routes =================================================================
+server.post('/user/register',
+  passport.authenticate('register'),
+  function (req, res) {
+    res.send(req.user);
+  })
+;
+
+server.post('/user/login',
+  passport.authenticate('login'),
+  function (req, res) {
+    res.send(req.user);
+  })
+;
+
+// GET Routes ==================================================================
 
 module.exports = server;
