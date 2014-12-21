a68acd235b0d1fe6fb22a19996b88dd0e4933dd4
asonnenschein <adrian.sonnenschein@gmail.com>
2014-12-20 23:39:52 -0700

    make sure auth is actually working

--- source/server.js
+++ source/server.js
@@ -10,27 +10,38 @@ var express = require('express')
 var server = express();
 server.set('port', process.env.PORT || 3030);
 
-require('./passport')(passport);
+server.use(cookieParser('secret'));
+server.use(session({secret: 'secret'}));
 
-server.use(cookieParser());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json({limit: '25mb'}));
 
-server.use(session({secret: 'secret', saveUninitialized: true, resave: true}));
+require('./passport')(passport);
 server.use(passport.initialize());
 server.use(passport.session());
+
+function checkAuthorization (req, res, next) {
+  if (req.isAuthenticated && req.user.username) {
+    return next();
+  }
+  else {
+    res.status(401).end();
+  }
+}
 
 // Login & Logout Routes =======================================================
 server.post('/api/v1/login',
   passport.authenticate('login'),
   function (req, res, next) {
-    res.send(req.user);
+    res.status(200).end();
   })
 ;
 
 server.post('/api/v1/logout',
+  checkAuthorization,
   function (req, res, next) {
-
+    req.logout();
+    res.status(200).end();
   })
 ;
 
@@ -43,12 +54,14 @@ server.post('/api/v1/users',
 ;
 
 server.put('/api/v1/users/:username',
+  checkAuthorization,
   function (req, res, next) {
 
   })
 ;
 
 server.delete('/api/v1/users/:username',
+  checkAuthorization,
   function (req, res, next) {
     return next();
   }, routes.deleteUser)
@@ -56,12 +69,21 @@ server.delete('/api/v1/users/:username',
 
 // Truck Routes ================================================================
 server.post('/api/v1/trucks',
+  checkAuthorization,
   function (req, res, next) {
 
   })
 ;
 
 server.put('/api/v1/trucks/:truck',
+  checkAuthorization,
+  function (req, res, next) {
+
+  })
+;
+
+server.delete('/api/v1/trucks/:truck',
+  checkAuthorization,
   function (req, res, next) {
 
   })
@@ -85,44 +107,42 @@ server.get('/api/v1/trucks/:user',
   })
 ;
 
-server.del('/api/v1/trucks/:truck',
-  function (req, res, next) {
-
-  })
-;
-
 // Menu Routes =================================================================
-server.post('/api/v1/menus',
+server.post('/api/v1/trucks/:truck/menus',
+  checkAuthorization,
   function (req, res, next) {
 
   })
 ;
 
-server.put('/api/v1/menus/:menu',
+server.put('/api/v1/trucks/:truck/menus/:menu',
+  checkAuthorization,
   function (req, res, next) {
 
   })
 ;
 
-server.get('/api/v1/menus',
+server.delete('/api/v1/trucks/:truck/menus/:menu',
+  checkAuthorization,
   function (req, res, next) {
 
   })
 ;
 
-server.get('/api/v1/menus/:menu',
+server.get('/api/v1/trucks/:truck/menus',
   function (req, res, next) {
 
   })
 ;
 
-server.del('/api/v1/menus/:menu',
+server.get('/api/v1/trucks/:truck/menus/:menu',
   function (req, res, next) {
 
   })
 ;
 
 // Geography Routes ============================================================
+
 
 module.exports = function (done) {
   db.sequelize.sync()
--- test/test.js
+++ test/test.js
@@ -1,12 +1,15 @@
 var supertest = require('supertest');
 
 describe('Foodie Wheels REST API Tests', function () {
-  var server;
+  var server
+    , agent
+    ;
 
   before(function (done) {
     require('../source/server')(function (err, app) {
       if (err) throw err;
       server = app;
+      agent = supertest.agent(server);
       done();
     })
   });
@@ -17,7 +20,7 @@ describe('Foodie Wheels REST API Tests', function () {
       "email": "email@test.com",
       "password": "testpassword"
     };
-    supertest(server)
+    agent
       .post('/api/v1/users')
       .send(request)
       .expect(200, done)
@@ -30,16 +33,36 @@ describe('Foodie Wheels REST API Tests', function () {
       "email": "email@test.com",
       "password": "testpassword"
     };
-    supertest(server)
+    agent
       .post('/api/v1/login')
       .send(request)
       .expect(200, done)
     ;
   });
 
+  it('POST logout from a user accout', function (done) {
+    agent
+      .post('/api/v1/logout')
+      .expect(200, done)
+    ;
+  });
+
+  it('POST register an account to delete', function (done) {
+    var request = {
+      "username": "testdeleteuser",
+      "email": "email@test.com",
+      "password": "testpassword"
+    };
+    agent
+      .post('/api/v1/users')
+      .send(request)
+      .expect(200, done)
+    ;
+  });
+
   it('DELETE a user account', function (done) {
-    supertest(server)
-      .delete('/api/v1/users/testuser')
+    agent
+      .delete('/api/v1/users/testdeleteuser')
       .expect(200, done)
     ;
   });
